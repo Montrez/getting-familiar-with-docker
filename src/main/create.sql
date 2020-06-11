d766de781bb8e769af4c1477ede4d0b0857582ae
Montrez <montrez.cox@gmail.com>
2020-06-10 23:39:15 -0400

    Create User controller and pojo information.

--- src/main/create.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `new_table` (
-  `name` varchar(40) NOT NULL,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ src/main/createSQL.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS `attorneygo_schema`;
+
+CREATE DATABASE `attorneygo_schema`;
+
+USE `attorneygo_schema`;
+
+CREATE TABLE `new_table` (
+  `user_id`		int(11)			NOT NULL	AUTO_INCREMENT,
+  `username` 	varchar(45)		NOT NULL,
+  `password`	varchar(45)		NOT NULL,
+  `name` 		varchar(40) 	NOT NULL,
+  `enabled`		bit(1)			NOT NULL,
+  PRIMARY KEY (`user_id`)
+);
+
+insert into new_table(username, password, name, enabled) values
+('cweems', 'weems', 'Charles Weems', 1),
+('mcox', 'cox', 'Montrez Cox', 1),
+('bderricho', 'derricho', 'Brandon Derricho', 1),
+('jtolson', 'tolson', 'Jack Tolson', 1),
+('fdallass', 'dallass', 'Francisca Dallass', 1),
+('smartin', 'martin', 'Shea Martin', 1);
